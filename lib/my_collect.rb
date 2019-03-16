220960a89b2b11f5d2bb9a2494ffa60662cdd349
Stephanie Purple <spurple133@gmail.com>
2019-03-16 01:00:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,8 @@ def my_collect(array)
   i=0
   new=[]
   while i < array.length
-    my_collect(array).do |lang|
-    lang.upcase
-    new << lang.upcase
+    new << yield(array[i])
     i +=1
   end
-  array
+  new
 end
